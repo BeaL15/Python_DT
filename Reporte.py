eb19b5088d3ba3399dd84e4985c1b0af04261dd9
BeaL15 <beatrizlucero21@hotmail.com>
2022-03-29 00:10:46 -0500

    Streamlit

---
+++ Reporte.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pandas_profiling
+import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
+from pandas_profiling import ProfileReport
+
+
+df = pd.read_excel("Imp_m&m.xlsx", na_values=['='])
+
+profile = ProfileReport(df, title="Importacion Chocolates", dataset={
+        "description": "Este es mi primer reporte con Streamlit",
+        "copyright_holder": "Análisis por Beatriz P.S",
+        "copyright_year": "2022"},
+    variables={
+        "descriptions": {
+           "Partida aduanera" : "Partida aduanera"
+        }
+    }
+)
+
+
+st.title("Reporte de Imp Chocolates")
+st.write(df)
+st_profile_report(profile)
